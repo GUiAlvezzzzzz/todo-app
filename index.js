cc118f4e278832c75e8b4245c02237d105c1e129
Guilherme Alves <guilherme.santos11@eaportal.org>
2023-11-28 08:00:19 -0300

    Criação - primeira view do projeto

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
 
-app.get = express()
-app.engine("handlebars", exphbs.engine())
-app.set('view engine', 'handlebars')
-
 const app = express()
 
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
 app.get("/",(requisicao,resposta) => {
-    resposta.send("Olá,Mundo!")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000")
+    console.log("Servidor rodando na porta 3000!")
 })
 
  
